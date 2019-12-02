e1089243368a02ee6da17133c33203b0f2d4e4a2
CompuStore <yandripro.98@gmail.com>
2019-12-01 23:16:11 -0500

    new arreglo

--- app/src/main/java/com/example/rutinafitnn/Main17Activity.java
+++ app/src/main/java/com/example/rutinafitnn/Main17Activity.java
@@ -2,7 +2,9 @@ package com.example.rutinafitnn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main17Activity extends AppCompatActivity {
 
@@ -11,4 +13,49 @@ public class Main17Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main17);
     }
+    public void Numero68(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main84Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero69(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main85Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero70(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main86Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero71(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main87Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero72(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main88Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero73(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main89Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero74(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main90Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero75(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main91Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero76(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main92Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero77(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main93Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero78(View view) {
+        Intent siguientE = new Intent(Main17Activity.this, Main94Activity.class);
+        startActivity(siguientE);
+    }
+
 }
