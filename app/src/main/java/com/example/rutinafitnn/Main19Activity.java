03274abf5c88cad8ab148d025f88865e39bed1cf
CompuStore <yandripro.98@gmail.com>
2019-12-01 22:51:44 -0500

    new arreglo

--- app/src/main/java/com/example/rutinafitnn/Main19Activity.java
+++ app/src/main/java/com/example/rutinafitnn/Main19Activity.java
@@ -2,7 +2,9 @@ package com.example.rutinafitnn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main19Activity extends AppCompatActivity {
 
@@ -11,4 +13,39 @@ public class Main19Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main19);
     }
+    public void Numero60(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main76Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero61(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main77Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero62(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main78Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero63(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main79Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero64(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main80Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero65(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main81Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero66(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main82Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero67(View view) {
+        Intent siguientE = new Intent(Main19Activity.this, Main83Activity.class);
+        startActivity(siguientE);
+    }
+
+
+
 }
