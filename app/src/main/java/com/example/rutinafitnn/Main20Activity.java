3f6fd3d2ab607d0532fb9f7625d8dbbc5827a834
CompuStore <yandripro.98@gmail.com>
2019-12-01 23:47:36 -0500

    new arreglo

--- app/src/main/java/com/example/rutinafitnn/Main20Activity.java
+++ app/src/main/java/com/example/rutinafitnn/Main20Activity.java
@@ -2,7 +2,9 @@ package com.example.rutinafitnn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main20Activity extends AppCompatActivity {
 
@@ -11,4 +13,14 @@ public class Main20Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main20);
     }
+    public void Numero88(View view) {
+        Intent siguientE = new Intent(Main20Activity.this, Main104Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero89(View view) {
+        Intent siguientE = new Intent(Main20Activity.this, Main105Activity.class);
+        startActivity(siguientE);
+    }
+
+
 }
