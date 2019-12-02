da71dda4460b99b13299fb955974d840885c274b
CompuStore <yandripro.98@gmail.com>
2019-12-01 22:04:51 -0500

    new arreglo

--- app/src/main/java/com/example/rutinafitnn/Main15Activity.java
+++ app/src/main/java/com/example/rutinafitnn/Main15Activity.java
@@ -2,7 +2,9 @@ package com.example.rutinafitnn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main15Activity extends AppCompatActivity {
 
@@ -11,4 +13,63 @@ public class Main15Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main15);
     }
+    public void Numero45(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero46(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero47(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero48(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero49(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero50(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero51(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }public void Numero52(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero53(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero54(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero55(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }public void Numero56(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero57(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero58(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+    public void Numero59(View view) {
+        Intent siguientE = new Intent(Main15Activity.this, Main60Activity.class);
+        startActivity(siguientE);
+    }
+
 }
